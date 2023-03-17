9708ba1f4b4b3b7752904e6ff9fbf4cf84270151
Raymond <robertkejn@gmail,com>
2023-03-17 13:42:26 +0400

    Initial commit 2

--- ht_3_7.1/ht_3_7.1.cpp
+++ ht_3_7.1/ht_3_7.1.cpp
@@ -2,14 +2,24 @@
 #include <vector>
 #include <algorithm>
 
+template<class T>
+std::vector<T> unify(std::vector<T> v) {
+    std::sort(v.begin(), v.end(), [](const int& a, const int& b)->bool {return a < b; });
+    auto i = std::unique(v.begin(), v.end());
+    v.erase(i, v.end());
+    return v;
+}
+
+
 int main()
 {
     std::vector<int> v = { 1,1,2,5,6,1,2,4 };
-    std::sort(v.begin(), v.end(), [](const int& a, const int& b)->bool {return a < b; });
+
     std::cout << "[IN]: ";
     std::for_each(v.begin(), v.end(), [](const int& a)->void {std::cout << a << " "; });
+
+    v = unify(v);
+
     std::cout << "\n[OUT]: ";
-    auto i = std::unique(v.begin(), v.end());
-    while (i != v.end()) i = v.erase(i);
     std::for_each(v.begin(), v.end(), [](const int& a)->void {std::cout << a << " "; });
 }
